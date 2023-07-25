9463f78b4880e035eb37ff8673bc0a4ee842123f
transfer_kk <kseysh123@naver.com>
2023-07-25 13:57:30 +0900

    feat: 카카오 소셜 로그인 api 추가 api https 적용 후 확인 필요

--- config/config/urls.py
+++ config/config/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('accounts/', include('dj_rest_auth.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('accounts/', include('accounts.urls')),
+]# 로그아웃 시에는 /accounts/logout으로 요청한다.
