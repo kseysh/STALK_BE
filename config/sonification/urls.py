7a336e1f3532e0b9a7fc015831ca47f4d9bed847
soozzang <soozzang@inha.edu>
2023-07-30 17:51:42 +0900

    feat: swagger url 수정

--- config/sonification/urls.py
+++ config/sonification/urls.py
@@ -5,23 +5,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Snippets API",
-        default_version='v1',
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
-
 urlpatterns = [
-    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('now_data/',now_data, name='now_data'),
     path('il_bong/', il_bong, name='il_bong'),
     path('data_to_sound/', data_to_sound, name='data_sound'),
