d00a9c6dc2852f2e7c49a301ab79b6aff2c5428e
transfer_kk <kseysh123@naver.com>
2023-08-03 17:22:13 +0900

    fix : conversion, sonification urls.py에 추가

--- config/config/urls.py
+++ config/config/urls.py
@@ -43,9 +43,9 @@ urlpatterns = [
     path('dj_rest-auth/registration/',include('dj_rest_auth.registration.urls')),
 
 
-    #path('conversion/', include('conversion.urls')),
+    path('conversion/', include('conversion.urls')),
     path('accounts/', include('accounts.urls')),
-    #path('sonification/', include("sonification.urls")),
+    path('sonification/', include("sonification.urls")),
 
     re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]# 로그아웃 시에는 /accounts/logout으로 요청한다.
