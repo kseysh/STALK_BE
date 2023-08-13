5c3563656ed16f76706766eb8add894935ca10c6
soozzang <soozzang@inha.edu>
2023-08-13 14:02:08 +0900

    fix: 오류수정

--- config/accounts/urls.py
+++ config/accounts/urls.py
@@ -6,6 +6,4 @@ urlpatterns = [
     path('kakao/callback/', views.kakao_callback),
     path('kakao/logout/', views.kakao_logout),
     path('userinfo/',views.check_jwt_user),
-    path('post/',views.post),
-
 ]
