b3c7344d6abaa2cf9706a15ceacb80d7785178d7
transfer_kk <kseysh123@naver.com>
2023-08-14 16:34:58 +0900

    feat : url 변경

--- config/accounts/urls.py
+++ config/accounts/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('kakao/callback/', views.kakao_callback),
     path('kakao/logout/', views.kakao_logout),
     path('userinfo/',views.user_info),
-    path('userinfo/',views.check_jwt_user),
-    path('tempuserinfo/',views.call_temp_user_info),
+    path('tempuserinfo/',views.temp_user_login),
     path('tempuserlogin/',views.temp_user_login),
 ]
