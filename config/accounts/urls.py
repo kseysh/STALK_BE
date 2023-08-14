80ea33282c921b288322dfc1ee47fc47cf66e92d
transfer_kk <kseysh123@naver.com>
2023-08-14 16:42:11 +0900

    pr : 받아라~

--- config/accounts/urls.py
+++ config/accounts/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('kakao/callback/', views.kakao_callback),
     path('kakao/logout/', views.kakao_logout),
     path('userinfo/',views.user_info),
-    path('tempuserinfo/',views.temp_user_login),
     path('tempuserlogin/',views.temp_user_login),
 ]
