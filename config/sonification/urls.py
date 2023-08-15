44b166eec44e80153dfd4ecf372b4117d7377c37
soozzang <soozzang@inha.edu>
2023-08-15 22:01:32 +0900

    feat: 시가총액 기준 나열 기능 추가 및 코드 수정

--- config/sonification/urls.py
+++ config/sonification/urls.py
@@ -7,12 +7,14 @@ from drf_yasg import openapi
 
 urlpatterns = [
     path('transaction_rank/',transaction_rank, name='transaction_rank'),
+    path('f_transaction_rank/',f_transaction_rank, name='f_transaction_rank'),
     path('now_data/',now_data, name='now_data'),
     path('f_now_data/',f_now_data, name='f_now_data'),
     path('day_data/', day_data, name='day_data'),
     path('a_day_data/', a_day_data, name='a_day_data'),
     path('f_day_data/', f_day_data, name='f_day_data'),
     path('minute_data/', minute_data, name='minute_data'),
+    path('hmm__minute_data/', hmm__minute_data, name='hmm__minute_data'),
     path('a_minute_data/', a_minute_data, name='a_minute_data'),
     path('f_a_day_data/', f_a_day_data, name='f_a_day_data'),
     path('f_a_minute_data/', f_a_minute_data, name='f_a_minute_data'),
