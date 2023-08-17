2b09d81d833939617e91bcb52bbb9fe6ff4529ee
transfer_kk <kseysh123@naver.com>
2023-08-17 19:21:28 +0900

    fix : url 수정

--- config/accounts/urls.py
+++ config/accounts/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('kakao/logout/', views.kakao_logout),
     path('userinfo/',views.user_info),
     path('tempuserlogin/',views.temp_user_login),
-    path('test403',views.test403),
+    path('test403/',views.test403),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
