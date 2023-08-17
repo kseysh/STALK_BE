ee0ed552446a8575aa7e5a3f780b4b7224878fff
transfer_kk <kseysh123@naver.com>
2023-08-17 18:51:38 +0900

    fix : 401 error 발생 기능 생성

--- config/accounts/urls.py
+++ config/accounts/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from accounts import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 urlpatterns = [
     path('kakao/login/', views.kakao_login),
@@ -7,4 +9,7 @@ urlpatterns = [
     path('kakao/logout/', views.kakao_logout),
     path('userinfo/',views.user_info),
     path('tempuserlogin/',views.temp_user_login),
+    path('test403',views.test403),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
